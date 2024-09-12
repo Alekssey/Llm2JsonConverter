c1aa69cb8a34b3e7738a19204fa37034d06a0318
Aleksey Ivanov <ivanov.aleks2000-ru@yandex.ru>
2024-09-12 12:10:31 +0300

    - add RequestObject class to receive messages from Sergey Morozov in controller - pass first tests with request strait from LLM.

--- src/main/java/ru/mpei/llmconverter/controller/ApiController.java
+++ src/main/java/ru/mpei/llmconverter/controller/ApiController.java
@@ -1,21 +1,25 @@
 package ru.mpei.llmconverter.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import ru.mpei.llmconverter.model.RequestObject;
 import ru.mpei.llmconverter.service.ApiService;
 
 @RestController
 @CrossOrigin
+@Slf4j
 public class ApiController {
     @Autowired
     private ApiService service;
 
     @PostMapping("/api/convert")
-    public String convertLLMResponse(@RequestBody String body) {
-        this.service.extract_data(body);
+    public String convertLLMResponse(@RequestBody RequestObject body) {
+        log.warn(body.getRequest());
+        this.service.extract_data(body.getRequest());
         return "hi";
     }
 }
